5088ff0d577ff61901d83ea2dee335cfe1fc7974
Jake Runzer <jakerunzer@gmail.com>
2022-07-07 15:49:51 -0700

    Update index.tsx

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
     <Page tw="p-8">
       <div tw="max-w-4xl w-full mx-auto">
         <header tw="mt-12 mb-12">
-          <h1 tw="font-bold text-5xl">monorepo.</h1>
+          <h1 tw="font-bold text-5xl">monorepo...</h1>
         </header>
 
         <code>
